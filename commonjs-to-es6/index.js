47390add40aa2056660b0499ae6dcbcbca8ed158
Tyler Vigario <TylerVigario90@gmail.com>
2023-06-05 10:44:28 -0700

    Change filenames to better reflect recipe

--- commonjs-to-es6/index.js
+++ commonjs-to-es6/index.js
@@ -1,5 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 import('n2words/i18n/en.js').then(({ default: n2words }) => {
     if (typeof n2words !== 'function' || n2words(1) !== 'one') {
         throw new Error('CommonJS dynamic import failed.');
